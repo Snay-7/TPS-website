1d1c5cd1c38bcc3e622299dfc4ddb6e9cfc0b2d6
Snay-7 <isnaydercandido@gmail.com>
2026-05-09 11:10:06 +0100

    Use service role key for server-side form submissions

--- lib/supabase/anon.ts
+++ lib/supabase/anon.ts
@@ -3,6 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 export function createAnonClient() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+    }
   );
 }
